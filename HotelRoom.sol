4c0e268e78053b21cdcd3b5bef0aa658146ef51a
mhmtnzly <65411256+mhmtnzly@users.noreply.github.com>
2021-04-28 01:21:17 +0200

    Create HotelRoom.sol

---
+++ HotelRoom.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.6.0;
+
+contract HotelRoom {
+    enum Statuses {Vacant, Occupied}
+    Statuses currentStatus;
+    
+    event Occupy(address _occupant, uint _value);
+    
+    address payable public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+        currentStatus=Statuses.Vacant;
+    }
+    
+    receive() external payable onlyWhileVacant costs(2 ether) {
+        currentStatus=Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender,msg.value);
+    }
+    
+    modifier onlyWhileVacant{
+        require(currentStatus == Statuses.Vacant, "Currently occupied.");
+        _;
+    }
+    
+    modifier costs(uint _amount) {
+        require(msg.value == _amount, "Not enough Ether provided.");
+        _;
+    }
+}
